c2e392b3cc1672b6701dd702d63386d29a41a526
icaca <icaca@live.com>
2024-03-12 01:55:06 +0800

    WCL Data Update

--- WclPlayerScore-Vanilla_DB5/10103.lua
+++ WclPlayerScore-Vanilla_DB5/10103.lua
@@ -3,14 +3,14 @@ return
 end
 
 STOP_Database = {
-["Quetzal"]="1平衡德,1恢复德",
+["Quetzal"]="1平衡,1恢复德",
 ["Feline"]="1射击猎",
-["Vurtz"]="1冰法,1火法",
+["Vurtz"]="1火法,1冰法",
 ["Fierabras"]="1惩戒骑,5奶骑",
 ["Arasuruv"]="1神牧,1暗牧",
 ["Sonntag"]="1奇袭贼",
 ["Saïsaï"]="1毁灭术",
-["Chogori"]="1防战,3狂暴战",
+["Chogori"]="1防战,3狂战",
 ["Kastan"]="2恢复德",
 ["Ferocia"]="2射击猎",
 ["Icysong"]="2火法",
@@ -18,7 +18,7 @@ STOP_Database = {
 ["Dontom"]="2神牧,2暗牧",
 ["Kaptenstab"]="2奇袭贼",
 ["Ouest"]="2毁灭术",
-["Kilpikirsi"]="2防战,5狂暴战",
+["Kilpikirsi"]="2防战,5狂战",
 ["Aloriane"]="3射击猎",
 ["Arcamon"]="3火法",
 ["Layercake"]="3奶骑",
@@ -26,7 +26,7 @@ STOP_Database = {
 ["Archpriest"]="3神牧,3暗牧",
 ["Lnz"]="3奇袭贼",
 ["Sakka"]="3毁灭术",
-["Sbp"]="1狂暴战,3防战",
+["Sbp"]="1狂战,3防战",
 ["Mikasa"]="4射击猎",
 ["Doobydoobydo"]="4火法",
 ["Mouratos"]="4奶骑",
@@ -34,16 +34,16 @@ STOP_Database = {
 ["Osiride"]="4暗牧,6神牧",
 ["Insta"]="4奇袭贼",
 ["Hellenis"]="4毁灭术",
-["Element"]="4狂暴战",
-["Nightqt"]="2狂暴战,4防战",
+["Element"]="4狂战",
+["Nightqt"]="2狂战,4防战",
 ["Crow"]="5射击猎",
 ["Jayme"]="5火法",
 ["Shaya"]="5神牧",
 ["Wy"]="5奇袭贼",
-["Gøodlife"]="5防战,6狂暴战",
+["Gøodlife"]="5防战,6狂战",
 ["Thørøs"]="6射击猎",
 ["Slukicz"]="6奇袭贼",
-["Ardx"]="6防战,7狂暴战",
+["Ardx"]="6防战,7狂战",
 ["Mitsui"]="7神牧",
 ["Skengman"]="7奇袭贼",
 ["Shøx"]="8奇袭贼",
@@ -51,5 +51,5 @@ STOP_Database = {
 }
 
 WP_Database = {
-["LASTUPDATE"]="2024-03-11"
+["LASTUPDATE"]="2024-03-12"
 }
