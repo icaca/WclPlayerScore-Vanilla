013f2c6c29c9c8f768adec9521e55f3b072962b8
icaca <icaca@live.com>
2024-03-24 01:55:04 +0800

    WCL Data Update

--- WclPlayerScore-Vanilla_DB3/10217.lua
+++ WclPlayerScore-Vanilla_DB3/10217.lua
@@ -85,5 +85,5 @@ STOP_Database = {
 }
 
 WP_Database = {
-["LASTUPDATE"]="2024-03-23"
+["LASTUPDATE"]="2024-03-24"
 }
--- WclPlayerScore-Vanilla_DB5/10075.lua
+++ WclPlayerScore-Vanilla_DB5/10075.lua
@@ -3,13 +3,14 @@ return
 end
 
 STOP_Database = {
-["Kowlana"]="1平衡,1恢复德",
+["Kowlana"]="1恢复德,1平衡",
+["Digedi"]="1野性德,1守护德,3恢复德",
 ["Jinzara"]="1射击猎",
 ["Jackyy"]="1奶骑,1惩戒骑",
 ["Moonchichi"]="1神牧,1暗牧",
 ["Burnie"]="1冰法,1奇袭贼,2火法",
 ["Freshjuice"]="1毁灭术",
-["Rabaukin"]="1防战,3狂战",
+["Mizane"]="1狂战,3防战",
 ["Habii"]="2平衡,2野性德,2守护德",
 ["Womanbearcat"]="2恢复德",
 ["Sebrella"]="2射击猎",
@@ -18,14 +19,14 @@ STOP_Database = {
 ["Thureugam"]="2暗牧,3神牧",
 ["Seiya"]="2奇袭贼",
 ["Jackyynator"]="2毁灭术",
-["Schmid"]="2狂战,2防战",
-["Digedi"]="1野性德,1守护德,3恢复德",
+["Schmid"]="2防战,2狂战",
 ["Elfenberg"]="3射击猎",
 ["Calurea"]="3火法",
 ["Cinnymaii"]="2神牧,3暗牧",
 ["Boppard"]="3奇袭贼",
-["Mizane"]="1狂战,3防战",
+["Rabaukin"]="1防战,3狂战",
 ["Nabenja"]="4奇袭贼",
+["Redsonja"]="4狂战,7防战",
 ["Furukama"]="4防战,8狂战",
 ["Minzli"]="5奇袭贼",
 ["Tigerspirit"]="5狂战",
@@ -33,11 +34,10 @@ STOP_Database = {
 ["Takoshi"]="6奇袭贼",
 ["Aìrox"]="6狂战",
 ["Thedeadmaker"]="6防战,7狂战",
-["Redsonja"]="4狂战,7防战",
 ["Jndy"]="9狂战",
 ["Medon"]="10狂战",
 }
 
 WP_Database = {
-["LASTUPDATE"]="2024-03-23"
+["LASTUPDATE"]="2024-03-24"
 }
