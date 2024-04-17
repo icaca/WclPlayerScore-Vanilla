e01641d539e0d3111fe6c0a116d3a551c78a6183
caca <icaca@live.com>
2024-04-17 08:34:05 +0800

    WCL daily updates

--- WclPlayerScore-Vanilla_DB4/10028.lua
+++ WclPlayerScore-Vanilla_DB4/10028.lua
@@ -3,13 +3,20 @@ return
 end
 
 STOP_Database = {
-["Fartnard"]="1火法,1冰法",
+["Fartnard"]="1冰法,1火法",
 ["Rossi"]="1神牧,1暗牧",
-["Mugorax"]="1恢复萨,1增强萨",
+["Mugorax"]="1增强萨,1恢复萨",
 ["Grandpop"]="1狂战",
 ["Tankuvm"]="2狂战",
 }
 
 WP_Database = {
+["Fartnard"]="AX:(火焰)1/0AT:(火焰)1/0|0",
+["Rossi"]="AX:(神圣)1/0AT:(神圣)1/0|0",
+["Pinchewuey"]="AT:(奇袭)1/0|0",
+["Mugorax"]="AX:(增强)1/0|0",
+["Onibaku"]="AT:(狂怒)1/0|0",
+["Tankuvm"]="AX:(狂怒)2/0|0",
+["Grandpop"]="AX:(狂怒)1/0|0",
 ["LASTUPDATE"]="2024-04-17"
 }
